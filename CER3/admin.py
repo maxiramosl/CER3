d614f54bbe1fdd51314a7814e3cab10bec6843d2
mobo <maximiliano.ramosl@usm.cl>
2023-11-27 21:18:57 -0300

    base de datos funcional y api le faltan permisos

--- CER3/admin.py
+++ CER3/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Usuario, Evento, Segmento
-admin.site.register(Usuario)
+from .models import UsuarioSegmento, Evento, Segmento, Tipo
+
+
+
+
+
+admin.site.register(UsuarioSegmento)
 admin.site.register(Evento)
 admin.site.register(Segmento)
+admin.site.register(Tipo)
 # Register your models here.
 # admin 123
--- CER3/models.py
+++ CER3/models.py
@@ -2,40 +2,36 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Segmento(models.Model):
-    nombre=models.CharField(max_length=30, primary_key=True)
+    nombre=models.CharField(max_length=30, primary_key=True, default="")
+    
+    def getNombre(self) -> str:
+        return self.nombre
     def __str__(self) -> str:
         return self.nombre
-
+    
+class Tipo(models.Model):
+    nombre=models.CharField(max_length=30, primary_key=True, default="")
+    def __str__(self) -> str:
+        return self.nombre
 
 class Evento (models.Model):
     fecha_inicio=models.DateField()
     fecha_termino=models.DateField()
     titulo=models.CharField(max_length=30, primary_key=True)
     descripcion=models.TextField()
-    tipo=[ ("Vacaciones"),
-          ("Feriado"),
-          ("Suspension de Actividades"),
-          ("Suspension de actividades PM"),
-          ("Periodo Lectivo"),
-          ("Suspension de evaluaciones"),
-          ("Ceremonia"),
-          ("EDDA"),
-          ("Evaluacion"),
-          ("Ayudantias"),
-          ("Hito Academico"),
-          ("Secretaria Academica"),
-          ("OAI"),
-    ]
+    tipo=models.OneToOneField(Tipo, on_delete=models.CASCADE)
     segmento=models.ManyToManyField(Segmento)
     def __str__(self) -> str:
         return self.titulo
     
-class Usuario(models.Model):
-    nombre=models.CharField(max_length=30, primary_key=True)
+class UsuarioSegmento(models.Model):
+    usuarios=models.OneToOneField(User, on_delete=models.CASCADE)
     segmento=models.ForeignKey(Segmento, on_delete=models.CASCADE)
-    def __str__(self) -> str:
-        return self.nombre
+    def getSegmento(self) ->str:
+        return self.segmento
+    
 
+    
 
 
 
